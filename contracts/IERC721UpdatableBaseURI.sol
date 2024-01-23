22a4bbe3346f0906f1bd832e1cc7aa099bf277c4
Toni Mateos <tmateos@freeverse.io>
2024-01-23 12:20:44 +0100

    fixed capitalization, added one help line

--- contracts/IERC721UpdatableBaseURI.sol
+++ contracts/IERC721UpdatableBaseURI.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.20;
 /**
  * @title Interface for ERC721 contracts which use a baseURI string
  *  to generate the tokenURI programmatically. It provides essential
- *  functions for managing the baseURI string.
+ *  functions for managing the baseURI string, as well as the prefix and
+ *  suffix strings placed before & after the tokenId to build the tokenURI
  * @dev The ERC-165 identifier for this interface is 0x418fb255
  * @author Freeverse.io, www.freeverse.io
  */
@@ -27,7 +28,7 @@ interface IERC721UpdatableBaseURI {
     event LockedBaseURI(string baseURI);
 
     /**
-     * @notice Event emitted on update of TokenId prefix and suffix
+     * @notice Event emitted on update of tokenId prefix and suffix
      * @param newPrefix the new string to be placed before the tokenId
      * @param newSuffix the new string to be placed after the tokenId
       */
