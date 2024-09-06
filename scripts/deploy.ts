9851b36ea567ef1e520c54998893018e74cd400c
Toni <>
2024-09-06 15:29:35 +0200

    Improving deploy script. Updating with LAOS Mainnet and LAOS Sigma

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,47 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+
+// To run this script:
+// $ npx hardhat run scripts/deploy.ts --network <network_name>
 
 async function main() {
+  const accounts = await ethers.getSigners();
+  const LAOSMainnetULOC = "https://uloc.io/GlobalConsensus(2)/Parachain(3370)/PalletInstance(51)";
+  const LAOSSigmaTestnetULOC = "GlobalConsensus(0:0x77afd6190f1554ad45fd0d31aee62aacc33c6db0ea801129acb813f913e0764f)/Parachain(4006)/PalletInstance(51)";
+
+  // Set the 4 params used in the constructor: onwerOfCollection, collectionName, tokenSymbol, and baseURI.
+  // For setting baseURI:
+  // - select between LAOSMainnetULOC / LAOSSigmaTestnetULOC
+  // - Make sure the baseURI ends with a slash character "/"
+
+  const ownerOfCollection = accounts[0].address;
   const collectionName = "Bridgeless Minting";
   const tokenSymbol = "LAETH";
-  const baseURI = "https://uloc.io/GlobalConsensus(3)/Parachain(3336)/PalletInstance(51)/AccountKey20(0xfffffffffffffffffffffffe0000000000000044)/";
-
-  const accounts = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", accounts[0].address);
+  const siblingCollectionInLAOS = "0xfFFFFFffFFFFFFFFfFFffFFe0000000000000002";
+  const baseURI = `${LAOSSigmaTestnetULOC}/AccountKey20(${siblingCollectionInLAOS})/`;
+  
+  console.log(`Deploying contract to ${network.name} with the account: ${accounts[0].address}`);
 
   const ContractFactory = await ethers.getContractFactory(
     "ERC721Universal",
   );
 
   const instance = await ContractFactory.deploy(
-    accounts[0].address,
+    ownerOfCollection,
     collectionName,
     tokenSymbol,
     baseURI,
   );
   await instance.waitForDeployment();
 
-  console.log(`Contract deployed to ${await instance.getAddress()}`);
+  const deployedContractAddress = await instance.getAddress();
+  console.log(`Contract deployed to ${deployedContractAddress}`);
+  console.log(`
+    Verifying the code. If ${network.name} is supported by the hardhat plugin, 
+    you can then verify the deployed code by adding the correspoing .env variable, and executing:
+  `);
+  console.log(`
+    npx hardhat verify --network ${network.name} "${deployedContractAddress}" "${ownerOfCollection}" "${collectionName}" "${tokenSymbol}" "${baseURI}"
+  `);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
