24893a4da0dbe80992cea3ef5140cdcb592845db
Toni <>
2024-11-28 17:25:05 +0100

    adding extra constructor params on deploy

--- test/ERC721Universal.ts
+++ test/ERC721Universal.ts
@@ -119,12 +119,12 @@ describe("ERC721Universal", function () {
     const deployedAddress = await erc721.getAddress();
     await expect(deployedTx)
       .to.emit(erc721, "NewERC721Universal")
-      .withArgs(deployedAddress, defaultURI);
+      .withArgs(deployedAddress, defaultURI, "laos-kitties", "LAK");
 
     // assert that the signature of the event (topic0) matches the expected value
     // first by computing it from the hash of the event type:
-    const expectedTopic0 = "0x74b81bc88402765a52dad72d3d893684f472a679558f3641500e0ee14924a10a";
-    const computedTopic0 = ethers.id("NewERC721Universal(address,string)");
+    const expectedTopic0 = "0xb4e1dc82d38c41e054f095ea5f4fc1d08bd9b949513f8ed2544ab8b47b1a8243";
+    const computedTopic0 = ethers.id("NewERC721Universal(address,string,string,string)");
     expect(computedTopic0).to.equal(expectedTopic0);
     // second by retrieving it from the TX directly
     const receipt = await deployedTx?.wait();
